c8672228a4123a17f3a984f5944a10c1ea8a973c
Liam Yates <156246617+yatesl4739@users.noreply.github.com>
2025-10-18 15:31:10 -0400

    basic comments

--- boilertrack/src/App.tsx
+++ boilertrack/src/App.tsx
@@ -1,19 +1,26 @@
+//This ts is for the POPUP
+//any js will run INTERNALLY within the popup window. This is not always the case, using chrome.scripting
 
 import gdgLogo from '/images/gdglogo128.png'
 
 import './App.css'
 
 function App() {
-  
+  //We can use document.body within a script being executed using scripting.executeScript to refer to the body doc of the page the user is currently on.
+  //for example:
   const onclick = async () => {
     let [tab] = await chrome.tabs.query({active: true});
+    //this following line, scripting.execute script, executes the following script in the specified web page
     chrome.scripting.executeScript({
+      //saying the script will execute on _ tab which was defined above as the currently active tab
       target: {tabId: tab.id!},
+      //its basically like running this in the console of the current webpage
         func: () => {
           document.body.style.backgroundColor = 'black';
         }
     });
   }
+  //any js in this html will run internally within the popup
   return (
     <>
       <div>
