558db1890c3ee66492ceaabbba14f9bd8ae13a4f
Liam Yates <156246617+yatesl4739@users.noreply.github.com>
2025-10-18 01:18:08 -0400

    created refrence for a service worker script

---
+++ service-worker.js
@@ -0,0 +1,8 @@
+//A service worker script is esentially a "background" script.
+//once the exension is installed, this will always be "dormant" in the computer
+//a service worker script is event driven, so we could use this script for something like
+//"when the user openes brightspace, scan" or something, so that the system is always scanning in the background
+
+
+//when the chrome extension is clicked:
+//chrome.action.onClicked.addListener();
