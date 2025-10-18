71a35de6ad1d1222f8236f283c85ef7690f31b02
Liam Yates <156246617+yatesl4739@users.noreply.github.com>
2025-10-18 01:35:30 -0400

    functions running on user tab

--- index.js
+++ index.js
@@ -12,3 +12,20 @@ async function sayHello() {
 
 //as I was saying above, this document is refering to the html doc of index.html (the popup HTML)
 document.getElementById("helloBtn").addEventListener("click",sayHello);
+//THIS IS NOT ALWAYS TRUE HOWEVER
+//We can use document.body within a script being executed using scripting.executeScript to refer to the body doc of the page the user is currently on.
+//for example:
+async function changeColor() {
+    let [tab] = await chrome.tabs.query({ active: true});
+    //this following line, scripting.execute script, executes the following script in the specified web page
+    chrome.scripting.executeScript({
+        //saying the script will execute on _ tab which was defined above as the currently active tab 
+        target: {tabId: tab.id},
+        //its basically like running this in the console of the current webpage
+        func: () => {
+            document.body.style.backgroundColor = 'red';
+        }
+    });
+}
+
+document.getElementById("makeItRed").addEventListener("click",changeColor);
