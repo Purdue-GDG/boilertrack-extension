9726d65403c6312e88d6b75a260610f4cb3c5626
Liam Yates <156246617+yatesl4739@users.noreply.github.com>
2025-11-07 22:13:45 -0500

    Resetting OCR branch to main branch for a fresh start at an attempt of tessert integration at the local level

--- boilertrack/vite.config.ts
+++ boilertrack/vite.config.ts
@@ -2,19 +2,7 @@ import manifest from './manifest';
 import { defineConfig } from 'vite';
 import { crx } from '@crxjs/vite-plugin';
 import react from '@vitejs/plugin-react-swc';
-import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [react(), crx({ manifest })],
-  build: {
-    rollupOptions: {
-      input: {
-        offscreen: resolve(__dirname, 'src/offscreen.ts'),
-      },
-      output: {
-        entryFileNames: '[name].js',
-        chunkFileNames: 'assets/[name]-[hash].js',
-      },
-    },
-  },
 });
